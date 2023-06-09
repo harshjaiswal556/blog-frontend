da26f14c4074b3ee4666a4ebac42d417c25f7dc7
harsh jaiswal <tech.harsh2003@gmail.com>
2023-06-10 02:35:31 +0530

    posting blog page done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,17 @@
-export default function Post(){
-    return(
-        <div className="post">
-        <div className="image">
-          <img src="https://img.mensxp.com/media/content/2022/Sep/googledocmediakixbfl6fn390jnu_63230eb41cb2d.jpeg" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Ways To Use Coffee For Tan Removal</h2>
-          <p className="info"><a className="author">Harsh Jaiswal</a>
-            <time>23-01-06 16:45</time></p>
-          <p className="summary">Coffee is easily available in every household and can be used to create your own face masks, face scrubs and body scrubs.</p>
-        </div>
+import { format } from "date-fns"
+
+export default function Post({ title, summary, cover, content, createdAt, author }) {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src={'http://localhost:4000/' + cover} alt="" />
       </div>
-    )
+      <div className="texts">
+        <h2>{title}</h2>
+        <p className="info"><a className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time></p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  )
 }
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Navigate } from 'react-router-dom'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -23,19 +24,30 @@ export default function CreatePost() {
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
     const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
     async function createPost(e) {
         const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
-        e.preventDefault();
         data.set('file', files[0]);
+        e.preventDefault();
         // console.log(files);
         const response = await fetch('http://localhost:4000/post', {
             method: 'POST',
             body: data,
+            credentials: 'include'
         })
-        console.log(await response.json());
+        if (response.ok) {
+            setRedirect(true);
+        } else {
+            alert('Please fill all the fields.')
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={"/"} />
     }
 
     return (
